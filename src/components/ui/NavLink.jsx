2cac249dc10cbc5424da94648a584ff854c0af72
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-06-08 08:55:22 +0530

    adding toggle menu logic

--- src/components/ui/NavLink.jsx
+++ src/components/ui/NavLink.jsx
@@ -1,49 +1,61 @@
 import { Link } from "react-router-dom";
 
-export default function NavLink() {
+export default function NavLink({isOpenState}) {
+
+  const {isOpen, setIsOpen} = isOpenState;
+
+
+  const menuItemClasses = `
+     w-full border-b-1 border-slate-200 px-4 py-3 cursor-pointer
+        hover:bg-[var(--accent-color)] hover:text-white 
+        md:hover:text-[var(--accent-color)]
+        md:border-none   
+        md:hover:bg-transparent
+  `;
   return (
     <div className="w-full md:w-fit flex flex-col items-start md:flex-row items-center ">
       <Link
         to="/about"
-        className="
-        w-full border-b-1 border-slate-200 px-4 py-3 cursor-pointer
-        hover:bg-[var(--accent-color)] hover:text-white 
-        md:hover:text-[var(--accent-color)]
-        md:border-none   
-        md:hover:bg-transparent"
+        className={`
+        ${menuItemClasses}
+        `}
+        onClick={() => {
+          setIsOpen(!isOpen)
+        }}
       >
         About
       </Link>
+
       <Link
         to="/careers"
-         className="
-        w-full border-b-1 border-slate-200 px-4 py-3 cursor-pointer
-        hover:bg-[var(--accent-color)] hover:text-white 
-        md:hover:text-[var(--accent-color)]
-        md:border-none   
-        md:hover:bg-transparent"
+        className={`
+        ${menuItemClasses}
+        `}
+         onClick={() => {
+          setIsOpen(!isOpen)
+        }}
       >
         Careers
       </Link>
       <Link
         to="/roadmaps"
-         className="
-        w-full border-b-1 border-slate-200 px-4 py-3 cursor-pointer
-        hover:bg-[var(--accent-color)] hover:text-white 
-        md:hover:text-[var(--accent-color)]
-        md:border-none   
-        md:hover:bg-transparent"
+        className={`
+        ${menuItemClasses}
+        `}
+         onClick={() => {
+          setIsOpen(!isOpen)
+        }}
       >
         Roadmaps
       </Link>
       <Link
         to="/quiz"
-         className="
-        w-full border-b-1 border-slate-200 px-4 py-3 cursor-pointer
-        hover:bg-[var(--accent-color)] hover:text-white 
-        md:hover:text-[var(--accent-color)]
-        md:border-none   
-        md:hover:bg-transparent"
+        className={`
+        ${menuItemClasses}
+        `}
+         onClick={() => {
+          setIsOpen(!isOpen)
+        }}
       >
         Quiz
       </Link>
--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -4,13 +4,28 @@ import NavLink from "../ui/NavLink";
 import ThemeToggle from "../ui/ThemeToggle";
 import logo from "../../assets/logo.png";
 import logoDark from "../../assets/logo-dark.png";
-import { Bookmark, TextAlignJustify, TrendingUp } from "lucide-react";
+import { Bookmark, TextAlignJustify, TrendingUp, X } from "lucide-react";
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
 
 export default function Navbar() {
   const { theme } = useContext(ThemeContext);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const menuContainerClasses = `
+        bg-white text-black  rounded mx-2
+
+        flex-col  gap-4
+        absolute z-10 top-full left-0 right-0
+  
+        light:text-black light:border light:border-slate-300
+        
+        md:w-2/3 md:relative 
+        md:flex-row md:justify-between  
+        md:bg-transparent  md:text-white md:border-none
+  `;
 
   return (
     <nav
@@ -29,31 +44,38 @@ export default function Navbar() {
           />
         </Link>
 
-        <div className="flex text-white md:hidden light:text-black">
-          <TextAlignJustify size={20} />
+        <div
+          className="flex text-white md:hidden light:text-black"
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          {isOpen ? <X size={20} /> : <TextAlignJustify size={20} />}
         </div>
 
         <div
-          className="
-        bg-white text-black  rounded mx-2
-
-        flex flex-col  gap-4
-        absolute z-10 top-full left-0 right-0
-  
-        light:text-black light:border light:border-slate-300
-        
-        md:w-2/3 md:relative 
-        md:flex-row md:justify-between  
-        md:bg-transparent  md:text-white md:border-none
-        "
+          className={`
+        ${isOpen ? "flex" : "hidden"}
+        ${menuContainerClasses}
+        `}
         >
-          <NavLink />
+          <NavLink isOpenState={{ isOpen, setIsOpen }} />
 
           <div className="flex items-center gap-4 p-4">
-            <Link to="/bookmarks">
+            <Link
+              to="/bookmarks"
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}
+            >
               <Bookmark size={20} />
             </Link>
-            <Link to="/progress-tracker">
+            <Link
+              to="/progress-tracker"
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}
+            >
               <TrendingUp size={20} />
             </Link>
             <ThemeToggle />
