77ba42331508addb823280309b14b1dacc39fbf8
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-06-12 10:36:23 +0530

    setup hero section

--- src/components/layout/HeroSection.jsx
+++ src/components/layout/HeroSection.jsx
@@ -1,33 +1,40 @@
 import Container from "./Container";
-import pathpilotHeroImg from  "../../assets/pathpilot-hero-img.png"
+// import pathpilotHeroImg from  "../../assets/pathpilot-hero-img.png"
+import heroBanner from "../../assets/hero-banner.png";
 
-
-
-export default function HeroSection () {
-
-
-    return (
-        <section>
-            <Container className="
+export default function HeroSection() {
+  return (
+    <section
+      style={{ backgroundImage: `url(${heroBanner})` }}
+      className="bg-cover  md:bg-center bg-no-repeat"
+    >
+      <Container
+        className="
             min-h-[600px] grid grid-cols-1 bg-transparent
 
             md:grid-cols-2
             
-            ">
+            "
+      >
+        <div className="flex flex-col justify-center items-start">
+          <h1 className="text-6xl font-semibold leading-16 md:leading-20  mb-5 light:text-white">
+            Discover Your Future, One Step at a Time.
+          </h1>
+          <p className="max-w-lg text-lg leading-8 light:text-white">
+            PathPilot helps students explore careers, discover interests, and
+            follow structured learning roadmaps to build confidence in their
+            future.
+          </p>
+          <div className="mt-10 flex gap-6">
+            <button className="primary-btn px-3 py-2.5">Explore Careers</button>
+            <button className="secondary-btn px-3 py-2.5 light:text-white">
+              Take Career Quiz
+            </button>
+          </div>
+        </div>
 
-                <div className="flex flex-col justify-center items-start">
-                    <h1 className="text-6xl font-semibold leading-20 mb-5">Discover Your Future, One Step at a Time.</h1>
-                    <p className="text-lg leading-8">PathPilot helps students explore careers, discover interests, and follow structured learning roadmaps to build confidence in their future.</p>
-                    <div className="mt-10 flex gap-6">
-                        <button className="primary-btn px-3 py-2.5">Explore Careers</button>
-                        <button className="secondary-btn px-3 py-2.5">Take Career Quiz</button>
-                    </div>
-                </div>
-
-                <div className="flex flex-col justify-center items-center">
-                    <img src={pathpilotHeroImg} alt="" />
-                </div>
-            </Container>
-        </section>
-    )
+        <div></div>
+      </Container>
+    </section>
+  );
 }
