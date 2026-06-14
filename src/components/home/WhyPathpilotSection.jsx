a277aa9330f6ecd8321f135d10fd11eb33fde183
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-06-14 20:17:37 +0530

    fix animation

--- src/components/home/WhyPathpilotSection.jsx
+++ src/components/home/WhyPathpilotSection.jsx
@@ -1,38 +1,45 @@
 import Container from "../layout/Container";
+import useFadeTop from "../../hooks/useFadeTop";
 
 
 export default function WhyPathpilotSection() {
 
 
+    const cardRef =  useFadeTop()
+
 
     return (
         <section>
             <Container className="
-            bg-red-600 min-h-[400px]
+            min-h-[500px]
             flex flex-col items-center justify-center
             ">
                 <h2 className="mb-5">Why PathPilot?</h2>
                 <div className="
                 w-full
-                grid grid-cols-1 gap-4
+                grid grid-cols-1 gap-10
                 md:grid-cols-3
-                ">
+                "
+                ref={cardRef}
+                >
                     <div className=" 
-                    bg-green-500 rounded-xl p-10
+                    shadow-xs shadow-white rounded-xl p-10
                     flex flex-col items-center justify-center
-                    ">
+                    "
+                    
+                    >
                         <h3 className="mb-2.5">Discover</h3>
                         <p className="text-center">Explore different career paths and understand what each profession involves.</p>
                     </div>
                     <div className=" 
-                    bg-green-500 rounded-xl p-10
+                    shadow-xs shadow-white rounded-xl p-10
                     flex flex-col items-center justify-center
                     ">
                         <h3 className="mb-2.5">Learn</h3>
                         <p className="text-center">Follow curated learning roadmaps designed for beginners.</p>
                     </div>
                     <div className=" 
-                    bg-green-500 rounded-xl p-10
+                    shadow-xs shadow-white rounded-xl p-10
                     flex flex-col items-center justify-center
                     ">
                         <h3 className="mb-2.5">Grow</h3>
---
+++ src/hooks/useFadeTop.js
@@ -0,0 +1,24 @@
+import { useRef } from "react"
+import { gsap, useGSAP } from "../lib/gsap"
+
+
+
+export default function useFadeTop() {
+
+
+    const ref = useRef(null);
+
+
+    useGSAP(() => {
+
+        gsap.from(ref.current, {
+            y: 100,
+            duration: 1.5,
+            ease: "power2.out",
+            stagger: 0.2,
+        })
+    })
+
+
+    return ref;
+}
