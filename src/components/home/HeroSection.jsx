5630664afa814c78d16de9bbd6bd2513d3f651e4
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-08-12 20:24:25 +0530

    fix size

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -29,8 +29,8 @@ export default function HeroSection() {
             
             "
       >
-        <div ref={heroRef} className="flex flex-col justify-center items-start">
-          <h1 className="!text-3xl md:!text-6xl  md:leading-16 md:leading-20  mb-5 light:text-black">
+        <div ref={heroRef} className=" flex flex-col justify-center items-start">
+          <h1 className="text-6xl  md:leading-16 md:leading-20  mb-5 light:text-black">
             Discover Your Future, <span className="text-violet-600">One Step</span> at a Time.
           </h1>
           <p className="max-w-lg text-lg leading-8 light:text-gray-500">
