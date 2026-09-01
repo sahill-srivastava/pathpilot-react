db987972c604a995823fdcce06fecc6a1abec1b6
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-09-01 20:26:08 +0530

    add cards section in careers detail

--- client/src/components/careerComp/CareerDetail.jsx
+++ client/src/components/careerComp/CareerDetail.jsx
@@ -9,9 +9,10 @@ const CareerDetail = () => {
 
   console.log(role);
 
-  console.log(careersData);
+  const {difficulty, duration, category} = role
+  const { eyebrow, title, subtitle, description, primaryCTA, secondaryCTA } = role.hero;
 
-  const { eyebrow, title, subtitle, description, difficulty, duration, primaryCTA, secondaryCTA } = role.hero;
+
   return (
     <>
       <section
@@ -23,7 +24,7 @@ const CareerDetail = () => {
                           flex flex-col items-center justify-center gap-2.5
                           "
         >
-          <div className="w-[330px] md:w-[800px] flex flex-col items-center text-center justify-center gap-2.5">
+          <div className="w-[330px] md:w-[800px] flex flex-col items-center text-center justify-center gap-4">
             <span
               className="bg-violet-900 light:text-white rounded-4xl mb-2.5 font-medium px-5 py-2.5 uppercase"
               style={{ fontSize: "13px" }}
@@ -33,11 +34,6 @@ const CareerDetail = () => {
             <h1 className="mb-2 md:mb-5 light:text-white">{title}</h1>
             <p>{subtitle}</p>
             <p>{description}</p>
-            <div className="flex gap-2">
-              <p>{difficulty}</p>
-              <span>I</span>
-              <p>{duration}</p>
-            </div>
             <div className="mt-5 md:mt-10 flex gap-6">
               <Link className="primary-btn px-3 py-2.5">{primaryCTA}</Link>
               <Link className="secondary-btn px-3 py-2.5">{secondaryCTA}</Link>
@@ -48,10 +44,30 @@ const CareerDetail = () => {
       <section>
         <Container
           className="
-               my-15
+               my-15 
                flex flex-col items-center justify-center
                "
-        ></Container>
+        >
+          <div className=" w-full grid grid-cols-1 md:grid-cols-4 gap-2.5">
+          <div className="bg-violet-600 rounded-2xl flex flex-col items-center p-10">
+            <h4>Difficulty</h4>
+            {difficulty}
+          </div>
+          <div className="bg-violet-600 rounded-2xl flex flex-col items-center p-10">
+            <h4>Duration</h4>
+            {duration}
+          </div>
+          <div className="bg-violet-600 rounded-2xl flex flex-col items-center p-10">
+            <h4>Career Type</h4>
+            <p>Devlopment</p>
+          </div>
+          <div className="bg-violet-600 rounded-2xl flex flex-col items-center p-10">
+            <h4>Category</h4>
+            {category}
+          </div>
+         
+          </div>
+        </Container>
       </section>
     </>
   );
