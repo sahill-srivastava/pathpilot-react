5bb0dd98ac75c00854ec5ce132cea2e8ffb7b65d
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-09-16 18:11:19 +0530

    section: add factsbox roadmap

--- client/src/components/roadmapComp/RoadmapDetail.jsx
+++ client/src/components/roadmapComp/RoadmapDetail.jsx
@@ -1,17 +1,19 @@
 import { useParams } from "react-router-dom";
-import roadmapsData from "../../mockdata/roadmap"
+import roadmapsData from "../../mockdata/roadmap";
 import HeroSectionDetail from "./HeroSectionDetail";
+import FactsBoxRoadmap from "./FactsBoxRoadmap";
 
 const RoadmapDetail = () => {
   const { slug } = useParams();
 
-  const role = roadmapsData.find(item => item.slug === slug)
-  console.log(role)
+  const role = roadmapsData.find((item) => item.slug === slug);
+  console.log(role);
   return (
     <>
-    <HeroSectionDetail data={role} />
+      <HeroSectionDetail data={role} />
+      <FactsBoxRoadmap data={role} />
     </>
-  )
+  );
 };
 
 export default RoadmapDetail;
