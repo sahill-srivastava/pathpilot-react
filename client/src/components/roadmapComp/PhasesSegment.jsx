68a24710a62c15696b7ee1255e3188d261639f24
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-09-16 21:08:06 +0530

    structuring phases section

--- client/src/components/roadmapComp/PhasesSegment.jsx
+++ client/src/components/roadmapComp/PhasesSegment.jsx
@@ -1,29 +1,55 @@
-import { Minus } from "lucide-react";
+import { Circle } from "lucide-react";
 import Container from "../layout/Container";
 
-const PhasesSegment = ({ id, data }) => {
+const PhasesSegment = ({ data }) => {
   const { phases } = data;
+  console.log(phases);
   return (
     <section>
       <Container
         className="
-                                 w-full h-full
-                                 flex flex-col items-start
-                                 "
+                  w-full h-full
+                  flex flex-col items-start
+                  "
       >
-        <div className="flex flex-col gap-4 items-start">
-          <h2 className="text-2xl light:text-white flex items-center gap-2.5 mb-1">Phase {id} <Minus /> {phases[id - 1].title}</h2>
-          <p><strong>Duration:</strong> {phases[id - 1].duration}</p>
-           <strong className="mt-4">Learn:</strong>
-          <ul className="flex flex-wrap gap-2">
-            {phases[id - 1].topics.map((r, index) => (
-              <li className="bg-white/20 py-1 px-2 rounded" key={index}>
-                {r}
-              </li>
-            ))}
-          </ul>
-          <strong className="mt-4">Goal:</strong>
-          <p>{phases[0].focus}</p>
+        <div className="w-full relative grid grid-cols-[1fr_auto_1fr] gap-20">
+          <div className="flex flex-col gap-20">
+            <div className="box h-[300px]  flex flex-col items-end justify-center text-4xl font-[var(--font-poppins)]">
+              Phase 1
+            </div>
+            <div className="box h-[300px] bg-violet-700 rounded-xl p-10 flex flex-col items-center justify-center"></div>
+          </div>
+
+          <div className="absolute left-[49%] top-[140px]"><Circle className="bg-white rounded-full" /></div>
+          <div>
+            <div className="w-[0.5px] h-[700px] bg-white rounded"></div>
+          </div>
+
+          <div className="flex flex-col gap-20">
+            <div className="box h-fit bg-violet-700 rounded-xl px-5 py-10 flex flex-col items-start justify-start gap-2.5">
+              <h3>{phases[0].title}</h3>
+              <p>
+                <strong>Duration: </strong>
+                {phases[0].duration}
+              </p>
+              <strong>Learn:</strong>
+              <ul className="flex flex-wrap gap-2">
+                {phases[0].topics.map((r, index) => (
+                  <li className="bg-violet-900 py-1 px-2 rounded" key={index}>
+                    {r}
+                  </li>
+                ))}
+              </ul>
+              <strong>Focus:</strong>
+               <p>
+                <strong>Duration: </strong>
+                {phases[0].focus}
+              </p>
+            </div>
+            <div className="box h-[300px] bg-transparent flex flex-col items-start justify-center text-4xl font-[var(--font-poppins)]">
+              Phase 2
+            </div>
+          </div>
         </div>
       </Container>
     </section>
--- client/src/components/roadmapComp/RoadmapDetail.jsx
+++ client/src/components/roadmapComp/RoadmapDetail.jsx
@@ -3,6 +3,7 @@ import roadmapsData from "../../mockdata/roadmap";
 import HeroSectionDetail from "./HeroSectionDetail";
 import FactsBoxRoadmap from "./FactsBoxRoadmap";
 import OverviewSegment from "./OverviewSegment";
+import PhasesSegment from "./PhasesSegment";
 
 const RoadmapDetail = () => {
   const { slug } = useParams();
@@ -14,6 +15,7 @@ const RoadmapDetail = () => {
       <HeroSectionDetail data={role} />
       <FactsBoxRoadmap data={role} />
       <OverviewSegment data={role} />
+      <PhasesSegment data={role} />
     </>
   );
 };
